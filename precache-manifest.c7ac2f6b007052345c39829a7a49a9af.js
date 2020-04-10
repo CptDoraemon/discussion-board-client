30c24ac47faa680e28ec9abaadf0a2809f8b82f4
Xiaoxi <yxx0827@gmail.com>
2020-04-09 22:07:49 -0400

    Updates

---
+++ precache-manifest.c7ac2f6b007052345c39829a7a49a9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc58e793ed594ee22c2a292507dd8f17",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "ee7f59f600cee2900603",
+    "url": "/news-app/static/css/main.5e30cc33.chunk.css"
+  },
+  {
+    "revision": "09fe626ca1b791a2f341",
+    "url": "/news-app/static/js/2.b470fba1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/news-app/static/js/2.b470fba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7f59f600cee2900603",
+    "url": "/news-app/static/js/main.08a85261.chunk.js"
+  },
+  {
+    "revision": "098e1a8017cf1774cac8",
+    "url": "/news-app/static/js/runtime-main.f13f5603.js"
+  }
+]);
