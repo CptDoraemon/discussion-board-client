22bcd73f20e29de126e9c671b19bb2fdcadeed4f
Xiaoxi <yxx0827@gmail.com>
2020-04-09 22:13:39 -0400

    Updates

---
+++ precache-manifest.94f343d4f9692254c2641f2dca261a4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62433fc06f23f614b3c915de2417a0fa",
+    "url": "/discussion-board-client/index.html"
+  },
+  {
+    "revision": "1e5026683b6f6dbd651e",
+    "url": "/discussion-board-client/static/css/main.5e30cc33.chunk.css"
+  },
+  {
+    "revision": "09fe626ca1b791a2f341",
+    "url": "/discussion-board-client/static/js/2.b470fba1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/discussion-board-client/static/js/2.b470fba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5026683b6f6dbd651e",
+    "url": "/discussion-board-client/static/js/main.1b1b06ce.chunk.js"
+  },
+  {
+    "revision": "6bb21778df24282b1ee8",
+    "url": "/discussion-board-client/static/js/runtime-main.81cc0595.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/news-app/precache-manifest.c7ac2f6b007052345c39829a7a49a9af.js"
+  "/discussion-board-client/precache-manifest.94f343d4f9692254c2641f2dca261a4c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/news-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/discussion-board-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
