b53a44d0fa7e76b9cc070f903618f975ad7e4879
Xiaoxi <yxx0827@gmail.com>
2020-10-14 16:17:29 -0400

    improve RouterScrollRestoration

--- src/utils/router-scroll-restoration.tsx
+++ src/utils/router-scroll-restoration.tsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import useQuery from "./use-query";
 
 export default function RouterScrollRestoration() {
-    const { pathname } = useLocation();
-    const queryParam = useQuery();
+    const { pathname, search } = useLocation();
 
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, [pathname, queryParam]);
+    }, [pathname, search]);
 
     return null;
 }
