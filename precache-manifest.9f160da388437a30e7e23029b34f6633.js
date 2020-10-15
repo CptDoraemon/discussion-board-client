e252d9972c0339c4987e94ad61788a24f6d14817
Xiaoxi <yxx0827@gmail.com>
2020-10-14 20:05:16 -0400

    Updates

--- precache-manifest.9f160da388437a30e7e23029b34f6633.js
+++ precache-manifest.9f160da388437a30e7e23029b34f6633.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e2483e66d092ea545ea13f5261fdc596",
+    "revision": "013898a96c141ef03050fa123b3f6081",
     "url": "/discussion-board-client/index.html"
   },
   {
-    "revision": "3cb0f56ac451f4e15eaf",
+    "revision": "314e5c0314eb9ec81758",
     "url": "/discussion-board-client/static/css/2.6d1032fe.chunk.css"
   },
   {
-    "revision": "aeae4566a12a762be9bf",
+    "revision": "6b50fabd87c4fba3afeb",
     "url": "/discussion-board-client/static/css/main.5e30cc33.chunk.css"
   },
   {
-    "revision": "3cb0f56ac451f4e15eaf",
-    "url": "/discussion-board-client/static/js/2.637b8308.chunk.js"
+    "revision": "314e5c0314eb9ec81758",
+    "url": "/discussion-board-client/static/js/2.39158229.chunk.js"
   },
   {
     "revision": "2ef428f39d34db82f69e2dde504832a3",
-    "url": "/discussion-board-client/static/js/2.637b8308.chunk.js.LICENSE.txt"
+    "url": "/discussion-board-client/static/js/2.39158229.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aeae4566a12a762be9bf",
-    "url": "/discussion-board-client/static/js/main.2c249c9f.chunk.js"
+    "revision": "6b50fabd87c4fba3afeb",
+    "url": "/discussion-board-client/static/js/main.e3986f27.chunk.js"
   },
   {
     "revision": "1e9cda47556fda47d71f",
