c6eed44f572b39d2d54c8fbf3b2d490954eb234e
Xiaoxi <yxx0827@gmail.com>
2022-09-19 19:14:29 -0400

    improve scroll restoration

--- src/utils/router-scroll-restoration.tsx
+++ src/utils/router-scroll-restoration.tsx
@@ -1,12 +1,19 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import {usePrevious} from "react-use";
 
 export default function RouterScrollRestoration() {
     const { pathname, search } = useLocation();
+    const previousPathname = usePrevious(pathname);
+    const previousSearch = usePrevious(search);
 
     useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname, search]);
+        if (previousSearch === undefined || previousPathname === undefined) {
+            return
+        }
 
-    return null;
+        if (previousSearch !== search || previousPathname !== pathname) {
+            window.scrollTo(0, 0);
+        }
+    }, [pathname, previousPathname, previousSearch, search])
 }
